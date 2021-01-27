47de50e463fbaa401a0f0597898cc8a6835465d0
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-01-27 10:02:26 -0300

    Math API: Operaçoes e constantes

---
+++ Math_API.js
@@ -0,0 +1,43 @@
+/*
+    ==== EXECUTAR TODOS OS COMANDOS NO CONSOLE DO NODE
+*/
+
+// --> Constantes
+Math.E;
+Math.LN10;
+Math.LN2;
+Math.LOG10E;
+Math.LOG2E;
+Math.PI;
+Math.SQRT1_2;
+Math.SQRT2;
+
+// --> Operações
+Math.abs(10); // Converte o sinal do número para positivo
+Math.abs(-10);
+Math.ceil(1.8); // Arredonda o número para cima
+Math.ceil(-1.8);
+Math.floor(7.9); // Arredonda o número para baixo
+Math.floor(-7.9);
+Math.round(4.5); // Se a parte decimal >=5 && <=9 ENTÃO arredonda pra cima, se não arredonda pra baixo
+Math.round(-4.5);
+Math.sign(5); // Retorna 1 se for positivo e 0 se for negativo
+Math.sign(-5);
+Math.trunc(2.3); // Elimina a parte decimal do número 
+Math.trunc(-2.3);
+
+// --> Operações Aritiméticas e Trigonométricas
+Math.cbrt(8);
+Math.cos(30);
+Math.exp(1);
+Math.hypot(4, 8);
+Math.log(1);
+Math.pow(2, 16);
+Math.sin(Math.PI / 2);
+Math.sqrt(49);
+Math.tan(Math.PI / 4);
+Math.min(1, 2, 4, 5, 66, 7);
+Math.max(2.3, 45, 45, 2, 3, 54);
+Math.random();
+Math.random() * 1000;
+Math.floor(Math.random() * 1000);
