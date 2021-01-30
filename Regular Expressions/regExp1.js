73c6efd59be7917caaf81217385e313b0edfbcf8
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-01-29 22:59:33 -0300

    Expressoes Regulares em JS - o  Basico

---
+++ Regular Expressions/regExp1.js
@@ -0,0 +1,4 @@
+let regExp = /^digengup@gmail.com$/; // O '^' indica com o que o valor deve começar na expressão, e o '$' indica como deve terminar (sem isso a expressão apenas considera que sua formação esteja em algum trecho do valor testado)
+let result = regExp.test("digengup@gmail.com");//testa se um dado valor está de acordo com a expressão regular 
+let result1 = regExp.exec("digengup@gmail.com");
+console.log(result1);
