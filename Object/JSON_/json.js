8b80b7a988fb20412007835721133dbe13ffab12
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-02-10 17:16:26 -0300

    JSON

---
+++ Object/JSON_/json.js
@@ -0,0 +1,7 @@
+JSON.stringify(10);
+JSON.stringify("Pthon Lang");
+JSON.stringify(true);
+JSON.stringify(false);
+JSON.stringify({ name: "Python", paradigm: "Multiparadigm" });
+JSON.stringify([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+JSON.stringify(null);
