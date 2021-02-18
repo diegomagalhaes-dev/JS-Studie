93a02388d51c0fba28df96e3ff218ccb5c99dae9
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-02-18 10:30:44 -0300

    Get and Set em JS.

---
+++ Function/get_set.js
@@ -0,0 +1,15 @@
+const rectangule = {
+    set tam(x) {
+        this.x = x;
+    },
+    set alt(y) {
+        this.y = y;
+    },
+    get area() {
+        return this.x * this.y;
+    }
+};
+
+rectangule.tam = 10;
+rectangule.alt = 2;
+console.log(rectangule.area);
