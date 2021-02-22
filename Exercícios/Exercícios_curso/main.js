703cf2961f7ba194d233e29d5f0f5bb62d6aa8ff
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-02-22 15:28:08 -0300

    Parte III exercício

--- Exercícios/Exercícios_curso/main.js
+++ Exercícios/Exercícios_curso/main.js
@@ -1,28 +1,35 @@
 // Part I
-const statement = "create table author (id number, name string, age number, city string, state string, country string)";
-const regExp = /create table ([a-z]+) \((.+)\)/;
-const parsedStatement = statement.match(regExp);
-const tableName = parsedStatement[1];
-let columns = parsedStatement[2];
-columns = columns.split(", ");
+
 
 // Part II
 const database = {
-    tables: {
-        [tableName]: {
+    tables: {},
+    // => Criação de um método através da "method notation", sem a verbosidade de "createTable: function (){}"
+    createTable(statement) {
+
+        const regExp = /create table ([a-z]+) \((.+)\)/;
+        const parsedStatement = statement.match(regExp);
+        const tableName = parsedStatement[1];
+        this.tables[tableName] = {
             columns: {},
             data: []
+        }
+        let columns = parsedStatement[2];
+        columns = columns.split(", ");
+        for (let column of columns) {
+            column = column.split(" ");
+            const name = column[0];
+            const type = column[1];
+            this.tables[tableName].columns[name] = type;
+        }
+    },
+    execute(statement) {
+        if (statement.startsWith("create table")) {
+            return this.createTable(statement);
         }
     }
 };
 
-for (let column of columns) {
-    column = column.split(" ");
-
-    const name = column[0];
-    const type = column[1];
-
-    database.tables[tableName].columns[name] = type;
-}
+database.createTable("create table author (id number, name string, age number, city string, state string, country string)");
 
 console.log(JSON.stringify(database, undefined, " "));
